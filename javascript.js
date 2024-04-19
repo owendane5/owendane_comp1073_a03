808a5d101849fca2902d77fa61c018ef47949909
owendane5 <112992614+owendane5@users.noreply.github.com>
2024-04-19 14:13:48 -0400

    Finished assignment starting styling

--- javascript.js
+++ javascript.js
@@ -2,17 +2,13 @@
 //Setting variables
 const apiUrlOne = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
 const apiUrlTwo = `https://pokeapi.co/api/v2/pokemon/`;
-const apiUrlThree =`fetch('https://official-joke-api.appspot.com/jokes/10')`;
+const apiUrlThree = `https://official-joke-api.appspot.com/jokes/`;
 const outputOne = document.getElementById('outputOne');
 const outputTwo = document.getElementById('outTwo');
 const outputThree = document.getElementById('outThree');
 
-
-
-
 //my first output this is to display the poke only
 //fetch method with error response (easier this way)
-
 
 function displayPokemonDefault() {
     fetch(apiUrlOne)
@@ -23,11 +19,11 @@ function displayPokemonDefault() {
             return response.json();
         })
         .then(data => {
-            //console.log('Pokemon Data:', data);  // Logging the data for Bulbasaur for testing
+            //console.log('Pokemon Data:', data);
 
             // Here I am just displaying the img for the pokemon
             const spriteUrl = data.sprites.front_default;
-            // Displaying the Pokémon's name and sprite in outputOne
+            // Displaying the info
             outputOne.innerHTML = `
             <h3>Pokemon Name: ${data.name}</h3>
             <img src="${spriteUrl}" alt="${data.name} Sprite"> `;
@@ -37,11 +33,15 @@ function displayPokemonDefault() {
             outputOne.textContent = 'Error fetching data';
         });
 
-
 }
+
+//calling default pokemon function
+displayPokemonDefault();
+
 //second function for displaying pokemon, this one will change the displayed pokemon by click of a button
 //most of the code is the same
 function displayPokemon(pokemonName) {
+    //using pokemonName as the deciding endpoint for the url based on the button pressed.
     const apiUrl = `${apiUrlTwo}${pokemonName}/`;
     fetch(apiUrl)
         .then(response => {
@@ -51,7 +51,9 @@ function displayPokemon(pokemonName) {
             return response.json();
         })
         .then(data => {
-            console.log('Pokemon Data:', data);
+           // console.log('Pokemon Data:', data);
+           
+           //getting img and displaying info
             const spriteUrl = data.sprites.front_default;
             outputTwo.innerHTML = `
                     <h3>Pokemon Name: ${data.name}</h3>
@@ -63,10 +65,32 @@ function displayPokemon(pokemonName) {
             outputTwo.textContent = 'Error fetching data';
         });
 }
+//third output to show two apis being used, finds joke by using the id property in the json
+//Most of code is same
 
-
-//calling default pokemon function
-displayPokemonDefault();
+function fetchJokeById(jokeId) {
+    //Using jokeId as the endpoint of url which is decided by text field.s
+    const apiUrl = `${apiUrlThree}${jokeId}`;
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network not available');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Joke Data:', data);
+            outputThree.innerHTML = `
+                <h3>Joke:</h3>
+                <p>${data.setup}</p>
+                <p>${data.punchline}</p>
+            `;
+        })
+        .catch(error => {
+            console.error('There was a problem fetching the joke:', error);
+            outputThree.textContent = 'Error fetching joke';
+        });
+}
 
 // Adding event listeners for my second output showing a pokemon based on what button is clicked
 
@@ -81,4 +105,11 @@ document.getElementById('btnTwo').addEventListener('click', () => {
 document.getElementById('btnThree').addEventListener('click', () => {
     displayPokemon('pikachu');
 });
+
+// Event listener for third output using id to find a joke
+getJoke.addEventListener('click', () => {
+    const jokeId = randomJoke.value;
+    fetchJokeById(jokeId);
+});
+
 
